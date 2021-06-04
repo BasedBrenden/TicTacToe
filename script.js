611f8791ec68c9d7a0403bb92ed2362e93e6fa05
Brenden Thomas <brenden.thomas17@gmail.com>
2021-06-04 16:03:35 -0500

    Fleshed out core factory function

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
-/*const Gameboard = {
-    let gameyboard = 
-    ['x','x','x',
-     'x','x','x',
-     'x','x','x',
-    ]
-};*/
-
 const ticGrid = document.querySelector('.ticGrid');
 
-function fillGrid(){
-    let i=0
-    do{
-        let square = document.createElement('DIV')
-        square.classList.add('square');
-        ticGrid.appendChild(square);
-        //square.innerHTML = "X";
-        i++;
-        square.setAttribute('value',i);
-    }while(i<9)
-};
+const gameFlow = () =>{
+    let lameboard = 
+            ['','','',
+             '','','',
+             '','',''];
 
-window.onload = fillGrid();
+    const addEvents = (test) =>{
+        test.addEventListener("click", function (e){
+            if(e.target.innerHTML != ''){ 
+                alert("Please choose a square that is empty");
+            }else{
+            e.target.innerHTML = 'X';
+            lameboard[e.target.id] = 'X';
+            console.log(lameboard[e.target.id]);
+            computerPlay();
+            }
+        })
+
+    }
+    const computerPlay = () =>{
+        let choice = Math.floor(Math.random() * 9);
+        let computerSquare = document.getElementById(choice);
+        if(computerSquare.innerHTML !=''){
+            do{
+                choice = Math.floor(Math.random() * 9);
+                computerSquare = document.getElementById(choice);
+            }while(computerSquare.innerHTML !='')
+        }
+        computerSquare.innerHTML = "O";
+        lameboard[choice] = "O";
+        console.log(lameboard[choice])
+    }
+    const fillGrid = () =>{
+        let i=0
+        do{
+            let square = document.createElement('DIV')
+            square.classList.add('square');
+            ticGrid.appendChild(square);
+            square.setAttribute('id',i);
+            i++;
+            addEvents(square);
+        }while(i<9)
+    };
+    
+    return{fillGrid, lameboard}
+}
+
+const Player = (name, symbol) => {
+    return{name, symbol}
+}
+
+
+
+const Julie = Player("Julie", 'x');
+
+console.log(Julie)
+const newGame = gameFlow();
+
+window.onload = newGame.fillGrid();
